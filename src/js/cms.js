05da20aefadd24a06118d80fda72977885a0335e
Yumin Chen <yumin@chen.so>
2025-04-17 14:09:17 +0000

    Replace `decap-cms-app` with `@sveltia/cms`

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,4 +1,4 @@
-import CMS from "decap-cms-app";
+import CMS from "@sveltia/cms";
 
 // Import main site styles as a string to inject into the CMS preview pane
 // eslint-disable-next-line import/no-unresolved
